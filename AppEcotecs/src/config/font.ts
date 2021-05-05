0aea14e307860386b6a314d7aeef46debece26a1
viniciushrk <viniciusrosa444@gmail.com>
2021-05-05 00:09:40 -0400

    Finish page serviceCatalgo

---
+++ AppEcotecs/src/config/font.ts
@@ -0,0 +1,3 @@
+export default{
+    text: 'Poppins_600SemiBold'
+};
--- appEcotec/App.tsx
+++ appEcotec/App.tsx
@@ -4,20 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import LogIn from './src/pages/LogIn/LogIn';
 import SignUp from './src/pages/SignUp/SignUp';
 import Success from './src/pages/Success/Success';
-import ServiceCatalog from './src/pages/ServiceCatalog/ServiceCatalog';
-import {useFonts,Poppins_600SemiBold,Poppins_100Thin_Italic} from '@expo-google-fonts/poppins';
-// import AppLoading from 'expo-app-loading';
-
-
-
 
 export default function App() {
-  const [fontsLoaded] = useFonts({Poppins_600SemiBold,Poppins_100Thin_Italic})
-
-  // if(!fontsLoaded)
-  // // return <AppLoading />
-
-  return <ServiceCatalog />;
+  return <LogIn />;
 }
 
 const styles = StyleSheet.create({
